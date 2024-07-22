349de0008d02b94c8279f933a46840064eeec6b7
Daniel Adler <daniela@cyera.io>
2024-07-22 18:13:43 +0300

    try to enrich event

--- event_enrichment/event_enrichment.py
+++ event_enrichment/event_enrichment.py
@@ -1,7 +1,7 @@
 import logging
 import json
 
-from jinja2 import Template
+from string import Template
 from typing import Dict, Any
 from collections import defaultdict
 from hikaru.model.rel_1_26.v1 import Pod, Job, CronJob
@@ -32,33 +32,22 @@ def event_pod_label_enricher(event: EventChangeEvent, params: PodLabelTemplate):
     except Exception as e:
         logger.error(f"Failed to log event: {e}")
 
-    # if not event.obj.regarding.kind == "Pod" or not event.obj.regarding.kind == "CronJob" or not event.obj.regarding.kind == "Job":
-    #     logger.info("Event is not regarding a pod / cronjob, skipping")
-    #     return
+    relevant_event_obj = None
 
     if event.obj.regarding.kind == "Pod":
         relevant_event_obj = Pod.readNamespacedPod(name=event.obj.regarding.name, namespace=event.obj.regarding.namespace).obj
-        if not relevant_event_obj:
-            logger.info("Pod not found, skipping")
-            return
     elif event.obj.regarding.kind == "CronJob":
         relevant_event_obj = CronJob.readNamespacedCronJob(name=event.obj.regarding.name, namespace=event.obj.regarding.namespace).obj
-        if not relevant_event_obj:
-            logger.info("CronJob not found, skipping")
-            return
     elif event.obj.regarding.kind == "Job":
         relevant_event_obj = Job.readNamespacedJob(name=event.obj.regarding.name, namespace=event.obj.regarding.namespace).obj
-        if not relevant_event_obj:
-            logger.info("Job not found, skipping")
-            return
-    else:
-        logger.info("Event is not regarding a pod, cronjob or a job, skipping")
+
+    if not relevant_event_obj:
+        logger.info("Pod not found, skipping")
         return
 
     logger.info("====================== relevant_event_obj ========================")
     logger.info(relevant_event_obj)
     logger.info("=========================================================")
-
 
     labels: Dict[str, Any] = defaultdict(lambda: "<missing>")
     labels.update(relevant_event_obj.metadata.labels)
@@ -66,6 +55,10 @@ def event_pod_label_enricher(event: EventChangeEvent, params: PodLabelTemplate):
     labels["name"] = relevant_event_obj.metadata.name
     labels["namespace"] = relevant_event_obj.metadata.namespace
     template = Template(params.template)
+
+    logger.info("====================== labels ========================")
+    logger.info(labels)
+    logger.info("=========================================================")
 
     logger.info("====================== template ========================")
     logger.info(template)
