4812fde2aff526e856876e2e854b80d172c25dfb
Daniel Adler <daniela@cyera.io>
2024-07-24 15:26:49 +0300

    test

--- event_enrichment/event_enrichment.py
+++ event_enrichment/event_enrichment.py
@@ -25,6 +25,9 @@ def event_pod_label_enricher(event: EventChangeEvent, params: PodLabelTemplate):
         relevant_event_obj = Pod.readNamespacedPod(name=event.obj.regarding.name, namespace=event.obj.regarding.namespace).obj
     elif event.obj.regarding.kind == "CronJob":
         relevant_event_obj = CronJob.readNamespacedCronJob(name=event.obj.regarding.name, namespace=event.obj.regarding.namespace).obj
+        logger.info('================================ CronJob ==================================')
+        logger.info(relevant_event_obj)
+        logger.info('==================================================================')
     elif event.obj.regarding.kind == "Job":
         relevant_event_obj = Job.readNamespacedJob(name=event.obj.regarding.name, namespace=event.obj.regarding.namespace).obj
 
