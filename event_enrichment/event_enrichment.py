0a5229d13c7368cb7aaac78aab05a1f019985ad5
Daniel Adler <daniela@cyera.io>
2024-08-15 14:45:35 +0300

    added pod oom killed enricher

--- event_enrichment/event_enrichment.py
+++ event_enrichment/event_enrichment.py
@@ -1,6 +1,6 @@
-from robusta.api import action, ActionParams, EventChangeEvent, MarkdownBlock, JobChangeEvent, JobStatus, TableBlock, RobustaPod
+from robusta.api import action, ActionParams, RobustaJob, EventChangeEvent, MarkdownBlock, JobChangeEvent, JobStatus, TableBlock, PodEvent, RobustaPod
 from hikaru.model.rel_1_26.v1 import Pod, Job, CronJob
-from typing import Dict, Any, List, Tuple
+from typing import Dict, Any, List, Tuple, Union
 from collections import defaultdict
 from string import Template
 import logging
@@ -14,7 +14,7 @@ class PodLabelTemplate(ActionParams):
     template: str
 
 
-def get_cluster_name(event: EventChangeEvent):
+def get_cluster_name(event: Union[EventChangeEvent, JobChangeEvent]) -> Union[str, None]:
     for sink in event.all_sinks.values():
         cluster_name = sink.registry.get_global_config().get("cluster_name")
         if cluster_name:
@@ -66,18 +66,10 @@ def event_pod_label_enricher(event: EventChangeEvent, params: PodLabelTemplate):
 
 @action
 def alert_job_labels_enricher(event: JobChangeEvent):
+    job_labels_keys_to_enrich = ["job-name", "team"]
     logger.info(f"Enriching JobChangeEvent event with job labels")
-    logger.info(f"Event -> {event}")
-    logger.info("=====================================")
-    logger.info("=====================================")
-    try:
-        logger.info(f"Event as dict -> {event.__dict__}")
-        logger.info("=====================================")
-        logger.info("=====================================")
-    except Exception as e:
-        logger.error(f"Error converting event to dict -> {e}")
 
-    job = event.get_job()
+    job: RobustaJob = event.get_job()
     if not job:
         logging.error(f"Cannot run alert_job_labels_enricher on event with no job: {event}")
         return
@@ -89,15 +81,16 @@ def alert_job_labels_enricher(event: JobChangeEvent):
         job_rows.append(["message", message])
 
     try:
-        job_labels = [[key, value] for key, value in job.metadata.labels.items()]
+        job_labels = __get_event_labels(job.metadata.labels, job_labels_keys_to_enrich)
+        job_rows.extend(job_labels)
+        image = job.get_single_pod().get_images()
+        if image:
+            job_rows.append(["image", str(image)])
     except Exception as e:
         logging.error(f"Error getting job labels -> {e}")
-        job_labels = []
 
-    job_rows.append(["name", job.metadata.name])
-    # job_rows.append(["image", container.image])
-
-    job_rows.extend(job_labels)
+    job_rows.append(["Name", job.metadata.name])
+    job_rows.append(["Namespace", job.metadata.namespace])
 
     table_block = TableBlock(
         job_rows,
@@ -106,15 +99,48 @@ def alert_job_labels_enricher(event: JobChangeEvent):
     )
     event.add_enrichment([table_block])
 
-    # if job_labels:
-    #     labels: Dict[str, Any] = defaultdict(lambda: "<missing>")
-    #     labels.update(job_labels)
-    #
-    #     logger.info(f"Enriching job alert with labels -> {labels}")
-    #
-    #     for sink in event.named_sinks:
-    #         for finding in event.sink_findings[sink]:
-    #             finding.subject.labels.update(labels)
+    cluster_name = get_cluster_name(event)
+
+    for sink in event.named_sinks:
+        for finding in event.sink_findings[sink]:
+            if cluster_name:
+                finding.subject.labels.update({"cluster": cluster_name})
+
+
+@action
+def pod_oom_killed_enricher(event: PodEvent):
+    pod_labels_keys_to_enrich = ["team"]
+    pod: RobustaPod = event.get_pod()
+    if not pod:
+        logging.error(f"Cannot run pod_oom_killer_enricher on event with no pod: {event}")
+        return
+
+    job_rows: List[List[str]] = [
+        ["Pod", pod.metadata.name],
+        ["Namespace", pod.metadata.namespace]]
+
+    try:
+        job_labels = __get_event_labels(event.obj.metadata.labels, pod_labels_keys_to_enrich)
+        job_rows.extend(job_labels)
+        image = pod.get_images()
+        if image:
+            job_rows.append(["Image", str(image)])
+    except Exception as e:
+        logging.error(f"Error getting pod labels -> {e}")
+
+    table_block = TableBlock(
+        job_rows,
+        ["description", "value"],
+        table_name="*Job information*",
+    )
+    event.add_enrichment([table_block])
+
+    cluster_name = get_cluster_name(event)
+
+    for sink in event.named_sinks:
+        for finding in event.sink_findings[sink]:
+            if cluster_name:
+                finding.subject.labels.update({"cluster": cluster_name})
 
 
 def __job_status_str(job_status: JobStatus) -> Tuple[str, str]:
@@ -129,3 +155,11 @@ def __job_status_str(job_status: JobStatus) -> Tuple[str, str]:
         return "Starting", ""
 
     return "Unknown", ""
+
+
+def __get_event_labels(event_labels: Dict, wanted_labels: List[str]) -> List[List]:
+    try:
+        return [[key, value] for key, value in event_labels.items() if key in wanted_labels]
+    except Exception as e:
+        logger.error(f"Error getting job labels -> {e}")
+        return []
